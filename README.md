c80e7ec0028ff69b0804b9265a195017ba705b31
Kushagra614 <kushagra619@gmail.com>
2025-01-27 22:51:11 +0530

    structure changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# HyperTradeX
--- src/utils/logger.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// function decleration
-
-void initialize_logger();  // Function to initialize the logger
-void log_example();   // Function to log some example messages
-
-
